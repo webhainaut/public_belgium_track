e12ff8b4f67fc38f2615ddc2bfd0d2fdf6631f70
webhainaut <webhainaut@gmail.com>
2024-02-07 18:43:13 +0100

    clean code

--- publicbelgiumtrack_all.py
+++ publicbelgiumtrack_all.py
@@ -8,49 +8,62 @@ from openpyxl.reader.excel import load_workbook
 from main.Arrest import Arrest
 from main.webscraper import WebScraper
 
-YEAR = 2023
-
-FILE_PATH_EXCEL = "result/CE VIe ch. {year}.xlsx"
-
 locale.setlocale(locale.LC_ALL, 'fr_BE.UTF-8')
 
 
 class PublicBelgiumTrack:
+    YEAR = 2023
+    FILE_PATH_EXCEL_FORMAT = "result/CE VIe ch. {year}.xlsx"
 
-    def __init__(self):
+    def __init__(self, file_path=FILE_PATH_EXCEL_FORMAT, year=YEAR):
         self.pd = pd
         self.load_workbook = load_workbook
+        self.webscraper = WebScraper()
+        self.file_path = file_path.format(year=year)
+        self.previous_df = None
+        self.begin_line = 0
+        self.last_arrest = None
 
-    def write_to_excel(self, arrests, file_path=FILE_PATH_EXCEL, begin_line=0):
+    def write_to_excel(self, arrests):
         df = self.pd.DataFrame([arrest.as_dict() for arrest in arrests])
         try:
-            with pd.ExcelWriter(file_path, engine='openpyxl', mode='a', if_sheet_exists="overlay") as writer:
-                df.to_excel(writer, sheet_name='Feuille1', index=False, startrow=begin_line, header=False)
+            with pd.ExcelWriter(self.file_path, engine='openpyxl', mode='a', if_sheet_exists="overlay") as writer:
+                df.to_excel(writer, sheet_name='Feuille1', index=False, startrow=self.begin_line, header=False)
+            print("remlir a partir de {begin_line} - last arrest : {arrest}".format(begin_line=self.begin_line,
+                                                                                    arrest=self.last_arrest.as_dict()))
         except FileNotFoundError:
-            with pd.ExcelWriter(file_path, engine='openpyxl', mode='w') as writer:
+            with pd.ExcelWriter(self.file_path, engine='openpyxl', mode='w') as writer:
                 df.to_excel(writer, sheet_name='Feuille1', index=False)
 
-    def read_from_excel(self, file_path=FILE_PATH_EXCEL):
-        if os.path.isfile(file_path):
-            return self.pd.read_excel(file_path)
+    def read_from_excel(self):
+        if os.path.isfile(self.file_path):
+            return self.pd.read_excel(self.file_path)
         else:
             print(f"Le fichier n'existe pas.")
+
+    def last_line(self):
+        if self.previous_df is not None:
+            return len(self.previous_df) + 1
+        return 0
+
+    def find_last_arrest(self):
+        self.previous_df = self.read_from_excel()
+        self.begin_line = self.last_line()
+        self.last_arrest = self.find_last_arrest_from_previous_df()
+
+    def find_last_arrest_from_previous_df(self):
+        if self.previous_df is not None:
+            return Arrest.from_dic(self.previous_df.tail(1).to_dict(orient='records')[0])
+
+    def get_arrests(self):
+        return self.webscraper.extract_arrets(self.YEAR, self.last_arrest)
 
 
 def main():
     public_belgium_track = PublicBelgiumTrack()
-    file_path = FILE_PATH_EXCEL.format(year=YEAR)
-    previous_df = public_belgium_track.read_from_excel(file_path)
-    last_arrest = None
-    begin_line = 0
-    if previous_df is not None:
-        last_arrest = Arrest.from_dic(previous_df.tail(1).to_dict(orient='records')[0])
-        begin_line = len(previous_df) + 1
-        print("remlir a partir de {begin_line} - last arrest : {arrest}".format(begin_line=begin_line,
-                                                                                arrest=last_arrest.as_dict()))
-    webscraper = WebScraper()
-    arrests = webscraper.extract_arrets(YEAR, last_arrest)
-    public_belgium_track.write_to_excel(arrests, file_path, begin_line)
+    public_belgium_track.find_last_arrest()
+    arrests = public_belgium_track.get_arrests()
+    public_belgium_track.write_to_excel(arrests)
 
 
 if __name__ == "__main__":
